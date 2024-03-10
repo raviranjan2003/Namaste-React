8ef59cc1adf8e8b0cf5f5f2081fbf5bfa5ff77a7
raviranjan2003 <2140131@sliet.ac.in>
2024-03-11 01:36:22 +0530

    functional component

--- laying-foundation/App.js
+++ laying-foundation/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
 //todo React.createElement => Object => When render to browser, it converted into HTML
 
@@ -8,20 +8,43 @@ import ReactDOM  from "react-dom/client";
 
 //! JSX => It is not HTML, it HTML like syntax
 
-//?This is not a pure javascript code, which understood by browser,
-//?first of all, it is transpiled before it reaches the JS engine
-//?transpiled ==> converted into ES6 which browser understand.
+//This is not a pure javascript code, which understood by browser,
+//first of all, it is transpiled before it reaches the JS engine
+//transpiled ==> converted into ES6 which browser understand.
 
-//?Transpiling is done by PARCEL, one more thing to keep in mind
-//? parcel is not doing this transpiling by it's own, it is a manager
-//? it appoint BABEL for the same job.
+//Transpiling is done by PARCEL, one more thing to keep in mind
+// parcel is not doing this transpiling by it's own, it is a manager
+// it appoint BABEL for the same job.
 
 //todo JSX => React.createElement() => JS Object => Converted into HTML
 
 const jsxHeading = <h1>Namaste React using JSX</h1>
 // console.log(jsxHeading);
 
+//? React Componet
+//todo Class Based Component - OLD
+
+//todo Functional Component - NEW
+//? A function which returns ReactElement is known as functional component.
+
+const Title = () => {
+    return (
+        <h1>Namaste React</h1>
+    )
+}
+const HeadingComponent = () => {
+    return (
+        <>
+            <Title />
+            <h1>Namaste React using functinal component.</h1>
+        </>
+    )
+}
+// same thing
+const HeadingComponent1 = () => <h1>Namaste React using functinal component.</h1>
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+// root.render(jsxHeading);
+root.render(<HeadingComponent />);
