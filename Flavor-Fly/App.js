ab122898925b1e0809468483b43e6237dcb84e3e
raviranjan2003 <2140131@sliet.ac.in>
2024-03-25 00:55:27 +0530

    props: dynamic card

--- Flavor-Fly/App.js
+++ Flavor-Fly/App.js
@@ -36,7 +36,8 @@ const Header = () => {
         </>
     )
 }
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+    const { name, type, rating } = props;
     return (
         <>
             <div className="res-card" style={{ backgroundColor : "#f0f0f0"}}>
@@ -44,9 +45,9 @@ const RestaurantCard = () => {
                 className="res-logo"
                 alt="res-logo" 
                 src="https://images.immediate.co.uk/production/volatile/sites/30/2013/05/spaghetti-carbonara-382837d.jpg?resize=768,574" />
-                <h3>Meghna Foods</h3>
-                <h4>Noodles, Chinese</h4>
-                <h4>4.5 stars</h4>
+                <h3>{name}</h3>
+                <h4>{type}</h4>
+                <h4>{rating} stars</h4>
             </div>
         </>
     )
@@ -57,12 +58,21 @@ const Body = () => {
             <div className="body">
                 <div className="search">Search</div>
                 <div className="res-container" >
-                    <RestaurantCard />
-                    <RestaurantCard />
-                    <RestaurantCard />
-                    <RestaurantCard />
-                    <RestaurantCard />
-                    <RestaurantCard />
+                    <RestaurantCard
+                        name = "Meghna food" 
+                        type = "Noodles"
+                        rating = "4.5"
+                    />
+                    <RestaurantCard 
+                        name = "KFC"
+                        type = "Chicken"
+                        rating = "4.6"
+                    />
+                    <RestaurantCard 
+                        name = "MacD"
+                        type = "Burger"
+                        rating = "4.4"
+                    />
                 </div>
             </div>
         </>
