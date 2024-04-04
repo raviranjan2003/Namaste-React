7344d4baee2e6d67331d1501de1616e8e922a388
raviranjan2003 <2140131@sliet.ac.in>
2024-04-05 00:53:31 +0530

    Navbar modified:responsiveness

--- Flavor-Fly/src/components/Header.jsx
+++ Flavor-Fly/src/components/Header.jsx
@@ -5,6 +5,10 @@ const Header = () => {
                 <div className="logo-container">
                     <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"/>
                 </div>
+                <div className="search-bar">
+                    <input type="text" placeholder="Search..." />
+                    <button type="submit">Search</button>
+                </div>
                 <div className="nav-items">
                     <ul>
                         <li>Home</li>
