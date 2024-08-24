04041d0764e9cc83bf13d22ae40a8c9e98c54954
raviranjan2003 <2140131@sliet.ac.in>
2024-08-24 23:34:50 +0530

    feat: fetching data from real API

--- Flavor-Fly/src/components/Body.jsx
+++ Flavor-Fly/src/components/Body.jsx
@@ -1,13 +1,32 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import RestaurantCard from './RestaurantCard';
 import { RestaurantList } from '../assets/restaurantList';
+import axios from 'axios';
 
 const Body = () => {
+    const URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
     const [resData, setResData] = useState(RestaurantList);
 
     const handleClick = () => {
         console.log("Button clicked");
         setResData(resData.filter(res => res.info.avgRating > 4.2));
+    }
+
+    useEffect(() => {
+        // console.log("UseEffect Hit==>");
+        fetchData();
+    }, [])
+    
+    const fetchData = async () => {
+        // const data = await axios.get(URL);
+        // console.log(data);
+        const data = await fetch(URL);
+        
+        const json = await data.json();
+
+        console.log(json);
+
+        
     }
 
     return (
