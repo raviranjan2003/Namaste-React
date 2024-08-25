dbef8ca632c464da5a2fbb67523113ee2138c5d9
raviranjan2003 <2140131@sliet.ac.in>
2024-08-25 23:54:20 +0530

    fix:bypass proxy without any plugin

--- Flavor-Fly/src/components/Body.jsx
+++ Flavor-Fly/src/components/Body.jsx
@@ -6,6 +6,7 @@ import Shimer from './Shimer';
 
 const Body = () => {
     const URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+    const PROXY_URL = "https://corsproxy.io/?"+ URL;
     const [resData, setResData] = useState([]);
 
     const [filteredRes, setFilteredRes] = useState([]);
@@ -30,7 +31,7 @@ const Body = () => {
     }, [])
     
     const fetchData = async () => {
-        const data = await fetch(URL);
+        const data = await fetch(PROXY_URL);
 
         const json = await data.json();
         // Optional Chaining
