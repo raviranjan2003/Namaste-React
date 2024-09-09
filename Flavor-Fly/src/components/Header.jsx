a7f417ba13acfeff28c302d09179c850dab90d99
raviranjan2003 <2140131@sliet.ac.in>
2024-09-10 03:01:03 +0530

    added Link for routing and updated styling

--- Flavor-Fly/src/components/Header.jsx
+++ Flavor-Fly/src/components/Header.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import img from "../assets/smoking-burger-with-lettuce-3624ld.png"
 
+import { Link } from "react-router-dom";
+
 const Header = () => {
 
     const [btnName, setBtnName] = useState("LogIn");
@@ -14,9 +16,9 @@ const Header = () => {
                 
                 <div className="nav-items">
                     <ul>
-                        <li>Home</li>
-                        <li>About Us</li>
-                        <li>Contact Us</li>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/about">About Us</Link></li>
+                        <li><Link to="/contact">Contact Us</Link></li>
                         <li>Cart</li>
                         <button style={{ padding : "5px 20px"}} onClick={() => {
                             btnName === "LogIn" ?
