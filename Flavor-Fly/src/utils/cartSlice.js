a64f4652882514cca43f2687ec6a275bd53035f4
raviranjan2003 <2140131@sliet.ac.in>
2025-01-31 23:50:10 +0530

    added cart slice

---
+++ Flavor-Fly/src/utils/cartSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+    name: "cart",
+    initialState: {
+        items: []
+    },
+    reducers: {
+        addItems: (state, action) => {
+            //mutating the state here
+            state.items.push(action.payload);
+        },
+        removeItem: (state) => {
+            state.items.pop();
+        },
+        clearCart: (state) => {
+            state.items.length = 0;
+        }
+    }
+})
+
+// console.log(cartSlice);
+
+// exporting actions
+export const { addItems, removeItem, clearCart } = cartSlice.actions;
+
+// exporting reducer
+export default cartSlice.reducer;
